848fbecd8ec10baf884315f8b921ea3e6ae00dc5
Jatin Assudaney <jatin.assudaney24@gmail.com>
2024-08-26 04:05:54 +0530

    style: add styles to post header

--- src/components/posts/posts-header.tsx
+++ src/components/posts/posts-header.tsx
@@ -3,7 +3,7 @@ const PostsHeader = () => {
   return (
     <header className="flex flex-col gap-3">
       <h2 className="text-2xl text-primary-text">Hello {username}</h2>
-      <p className="text-base text-secondary-text">
+      <p className="text-base text-secondary-text sm:max-w-[580px]">
         How are you doing today? Would you like to share something with the
         community 🤗
       </p>
